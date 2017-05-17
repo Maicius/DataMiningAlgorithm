ff438b67a39eee055954da330260094329d1a958
mai ciu s <maicius@outlook.com>
2017-05-17 17:27:13 +0800

    测试完成，正确率69%

--- main.py
+++ main.py
@@ -2,70 +2,87 @@
 from __future__ import division
 from numpy import *
 import operator
+import math
 import re
 
 def createDataset():
-    i = 0
-    data = []
     dataSet = []
-    i=0
-    j=4
+    trainDataSet = []
+    testDataSet = []
     file = open("titanic.dat", "r")
     for line in file.readlines()[8:]:
         data =  re.split(',|', line)
         #remove '\r \n' in list
         data = map(lambda x: x.strip(), data)
         dataSet.append(data)
-        i = i + 1
-    print dataSet
-    for k in range(i):
-
-        dataSet[k][0] = float(dataSet[k][0])/(0.965 + 1.87)
-        dataSet[k][1] = float(dataSet[k][1])/(4.38 + 0.228)
-        dataSet[k][2] = float(dataSet[k][2])/(0.521 + 1.92)
-    print "归一化："
-    print dataSet
-
-def classify(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
-    sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
-
-    classCount = {}
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    #print dataSet
+    for k in range(1400):
+        dataSet[k][0] = (float(dataSet[k][0]) + 1.87)/(0.965 + 1.87)
+        dataSet[k][1] = (float(dataSet[k][1]) + 0.228)/(4.38 + 0.228)
+        dataSet[k][2] = (float(dataSet[k][2]) + 1.92)/(0.521 + 1.92)
+        dataSet[k][3] = float(dataSet[k][3])
+        trainDataSet.append(dataSet[k])
+    for k in range(1401, 2200):
+        dataSet[k][0] = (float(dataSet[k][0]) + 1.87) / (0.965 + 1.87)
+        dataSet[k][1] = (float(dataSet[k][1]) + 0.228) / (4.38 + 0.228)
+        dataSet[k][2] = (float(dataSet[k][2]) + 1.92) / (0.521 + 1.92)
+        dataSet[k][3] = float(dataSet[k][3])
+        testDataSet.append(dataSet[k])
+    #print "train："
+    file.close()
+    print "训练数据:"
+    print trainDataSet
+    print "测试数据:"
+    print testDataSet
+    return trainDataSet, testDataSet
 
 
-def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
-
-    m = dataSet.shape[0]
-    normDataSet = dataSet - tile(minVals, (m, 1))
-    # print normDataSet
-    normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide
-    # print normDataSet
-    return normDataSet, ranges, minVals
+def caculateDiff(trainDataSet, testDataSet, k):
+    success = 0
+    count = 0
+    print "训练数据:"
+    print trainDataSet
+    print "测试数据:"
+    print testDataSet
+    diffArray = []
+    for testData in testDataSet:
+        count  = count + 1
+        print count
+        for trainData in trainDataSet:
+            #使用欧式方法计算距离
+            diff = math.sqrt((testData[0] - trainData[0])**2 + (testData[1] - trainData[1])**2 + (testData[2] - trainData[2])**2)
+            diffArray.append([diff, trainData[3]])
+            #print diff
+        #根据距离进行排序
+        diffArray = sorted(diffArray, key = lambda x:x[0])
+        countS = 0
+        countF = 0
+        for i in range(k):
+            if diffArray[i][1] == 1.0:
+                countS = countS + 1
+            elif diffArray[i][1] == -1.0:
+                countF =countF + 1
+        if countS > countF:
+            if testData[3] == 1.0:
+                success = success + 1
+        else:
+            if testData[3] == -1.0:
+                success = success + 1
+    print "测试数据共:"+ str(count) + "个"
+    print "预测成功数据：" + str(success) + "个"
+    return float(success) / float(count)
 
 
 def result():
-    createDataset()
-    datingDataMat, datingLabels = createDataset()  # load data setfrom file
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    book = 199
-    film = 4
-    print 'watch book ' + '199' + ' film' + ' 4'
-    print 'you are the person belong to'
-    print classify((301, 4), normMat, datingLabels, 1)
+    trainDataSet, testDataSet = createDataset()
+    print "finish to create dataSet"
+    print "Please input K:"
+    k = raw_input()
+    print "begin..."
+    successRate = caculateDiff(trainDataSet, testDataSet, int(k))
+    print "finish"
+    print "预测成功率为："
+    print successRate
 
 
 if __name__ == '__main__':
