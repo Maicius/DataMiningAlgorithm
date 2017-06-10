a57e0d20a4e17351cd80eae99cf205147e21402f
mai ciu s <maicius@outlook.com>
2017-06-10 19:55:59 +0800

    梯度下降和随机梯度下降算法细节修改

--- gradAscent.py
+++ gradAscent.py
@@ -1,13 +1,13 @@
 #coding=utf-8
-import NumPy
+import numpy as np
 import math
 import random
 
 def sigmoid(x):
-    return x * (1 - x)
+    return x
 
 def gradAscent(dataMatIn, classLabels):
-    dataMatrix = mat(dataMatIn)             #convert to NumPy matrix
+    dataMatrix = np.mat(dataMatIn)             #convert to NumPy matrix
     labelMat = np.mat(classLabels).transpose() #convert to NumPy matrix
     m,n = np.shape(dataMatrix)
     alpha = 0.001
@@ -16,40 +16,45 @@ def gradAscent(dataMatIn, classLabels):
     for k in range(maxCycles):              #heavy on matrix operations
         h = sigmoid(dataMatrix*weights)     #matrix mult
         error = (labelMat - h)              #vector subtraction
-        weights = weights + alpha * dataMatrix.transpose()* error #matrix mult
-    print weights
+        weights = weights + alpha * dataMatrix.transpose() * error #matrix mult
+    print "梯度下降:" + str(weights)
     return weights
 
 def stocGradAscent0(dataMatrix, classLabels):
+    dataMatrix = np.mat(dataMatrix)
     m,n = np.shape(dataMatrix)
+
     alpha = 0.01
-    weights = np.ones(n)   #initialize to all ones
+    weights = np.ones((n, 1))   #initialize to all ones
     for i in range(m):
-        h = sigmoid(sum(dataMatrix[i]*weights))
+        h = sigmoid(dataMatrix[i]*weights)
         error = classLabels[i] - h
-        weights = weights + alpha * error * dataMatrix[i]
+        weights = weights + alpha * dataMatrix[i].transpose() * error
+    print "随机梯度下降1:" + str(weights)
     return weights
+
 
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = np.shape(dataMatrix)
-    weights = np.ones(n)   #initialize to all ones
+    weights = np.ones((n, 1))   #initialize to all ones
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
             alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not
-            randIndex = int(random.uniform(0,len(dataIndex)))  #随机选取样本
-            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            randIndex = int(random.uniform(0, len(dataIndex)))  #随机选取样本
+            h = sigmoid((dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
             del(dataIndex[randIndex])#删除所选的样本
+    print "随机梯度下降2:" + str(weights)
     return weights
 
 if __name__ == '__main__':
     cases = [
-        [0, 0],
-        [0, 1],
-        [1, 0],
+        [1, 1],
         [1, 1],
     ]
-    labels = [[0], [1], [0], [1]]
+    labels = [1, 1]
     gradAscent(cases, labels)
+    stocGradAscent0(cases, labels)
+    stocGradAscent1(cases, labels)
